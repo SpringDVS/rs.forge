0a67c412db78e264ac1e8c2bb424308dae31aa6c
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-04-07 10:05:25 +0100

    forge: Packet forge based off large config struct
    This enables a comprehensive construction of packets to send to the
    server

--- src/main.rs
+++ src/main.rs
@@ -6,39 +6,112 @@ use spring_dvs::protocol::{Packet, PacketHeader, FrameRegister, FrameResponse};
 use std::env;
 use std::net::UdpSocket;
 
+struct config {
+	msg_type: DvspMsgType,
+	msg_target: String,
+
+	node_register: bool,
+	node_type: DvspNodeType,
+	node_state: DvspNodeState,
+	node_service: DvspService,
+	
+	text_content: String,
+	
+}
+
+impl config {
+	fn new() -> config {
+		config {
+			msg_type: DvspMsgType::GsnRegistration,
+			msg_target: "127.0.0.1:55301".to_string(),
+			
+			node_register: true,
+			node_type: DvspNodeType::Org,
+			node_state: DvspNodeState::Enabled,
+			node_service: DvspService::Dvsp,
+			
+			text_content: String::new(),
+		}
+	}
+}
+
 fn modify_msg_type(arg: &str ) -> DvspMsgType {
 	 
 	match arg {
-		"GsnRegistration" => DvspMsgType::GsnRegistration,
-		"GsnResponse" => DvspMsgType::GsnResponse,
+		"gsn_registration" => DvspMsgType::GsnRegistration,
+		"gsn_response" => DvspMsgType::GsnResponse,
 		_ => DvspMsgType::Undefined
 	}
 }
 
+fn modify_node_register(arg: &str) -> bool {
+	match arg {
+		"true" => true,
+		_ => false,
+	}
+}
+
+fn modify_node_type(arg: &str) -> DvspNodeType {
+	match arg {
+		"org" => DvspNodeType::Org,
+		"root" => DvspNodeType::Org,
+		_ => DvspNodeType::Undefined,
+	}
+}
+
+fn modify_node_state(arg: &str) -> DvspNodeState {
+	match arg {
+		"enabled" => DvspNodeState::Enabled,
+		"disabled" => DvspNodeState::Disabled,
+		"unresponsive" => DvspNodeState::Unresponsive,
+		_ => DvspNodeState::Unspecified,
+	}
+}
+
+fn modify_node_service(arg: &str) -> DvspService {
+	match arg {
+		"dvsp" => DvspService::Dvsp,
+		"http" => DvspService::Http,
+		_ => DvspService::Undefined,
+	}
+}
+
 enum ArgState {
-	None, MsgType, MsgContent, Target
+	None, MsgType, TextContent, MsgTarget, 
+	NodeRegister, NodeType, NodeState, NodeService
 }
 
 fn main() {
-	let mut msg_type = DvspMsgType::GsnRegistration;
-	let mut msg_target: String =  "127.0.0.1:55301".to_string();
-	let mut msg_content = String::new();
+	let mut cfg = config::new();
+	
 	let mut state: ArgState = ArgState::None;
 	
 	for a in env::args() {
 		
 		match a.as_ref() {
 			
-			"--type" => { state = ArgState::MsgType; },
-			"--target" => { state = ArgState::Target; },
-			"--content" => { state = ArgState::MsgContent; },
+			"--msg-type" => { state = ArgState::MsgType; },
+			"--msg-target" => { state = ArgState::MsgTarget; },
+			
+			
+			"--node-type" => { state = ArgState::NodeType; },
+			"--node-register" => { state = ArgState::NodeRegister; },
+			"--node-service" => { state = ArgState::NodeService; },
+			"--node-state" => { state = ArgState::NodeState; },
+			
+			"--text-content" => { state = ArgState::TextContent; },
 			
 			_ => {
 				
 				match state {
-					ArgState::MsgType => { msg_type = modify_msg_type(a.as_ref()); }
-					ArgState::Target => { msg_target = a; }
-					ArgState::MsgContent => { msg_content = a; }
+					ArgState::MsgType => { cfg.msg_type = modify_msg_type(a.as_ref()); },
+					ArgState::MsgTarget => { cfg.msg_target = a; },
+					ArgState::TextContent => { cfg.text_content = a; },
+					
+					ArgState::NodeRegister => { cfg.node_register = modify_node_register(a.as_ref()); },
+					ArgState::NodeService => { cfg.node_service = modify_node_service(a.as_ref()); },
+					ArgState::NodeType => { cfg.node_type = modify_node_type(a.as_ref()); },
+					ArgState::NodeState => { cfg.node_state = modify_node_state(a.as_ref()); },
 					_ => { }
 				};
 				
@@ -48,7 +121,8 @@ fn main() {
 		
 	}
 	
-	let bytes = forge_packet(msg_type, &msg_content);
+	let bytes = forge_packet(&cfg);
+	println!("<< out.bytes.len: {}", bytes.len());
 	let socket = match UdpSocket::bind("0.0.0.0:55045") {
 		Ok(s) => s,
 		Err(e) => {
@@ -57,7 +131,7 @@ fn main() {
 		}
 	};
 	
-	let m : &str = msg_target.as_ref();
+	let m : &str = cfg.msg_target.as_ref();
 	
     match socket.send_to(bytes.as_ref(), m) {
     	Ok(_) => { },
@@ -73,20 +147,23 @@ fn main() {
 		},
 	};
    	
-   	decode_packet(&bytes[..sz])
+   	decode_packet(&bytes[..sz]);
+   	println!("");
 }
 
-fn forge_packet(t: DvspMsgType, c: &str) -> Vec<u8> {
-	let bytes = match t {
+fn forge_packet(cfg: &config) -> Vec<u8> {
+	let bytes = match cfg.msg_type {
 		DvspMsgType::GsnRegistration => {
-			let f = FrameRegister::new(true, DvspNodeType::Org as u8, DvspService::Dvsp, String::from(c));
+			let f = FrameRegister::new(cfg.node_register, cfg.node_type as u8, cfg.node_service, cfg.text_content.clone());
 			f.serialise()
 		},
 		_ => { Vec::new() }
 	};
 	
-	let mut p = Packet::new(t);
+	let mut p = Packet::new(cfg.msg_type);
 	p.write_content(&bytes);
+	
+	println!("<< out.packet.msg_size: {}", p.header().msg_size);
 	
 	p.serialise()
 }
@@ -100,8 +177,10 @@ fn decode_packet(bytes: &[u8]) {
 			return; 
 		}
 	};
-	println!("byte.len: {}", bytes.len());
-	println!("Packet.content.size: {}", p.header().msg_size);
+	println!(">> in.bytes.len: {}", bytes.len());
+	println!(">> in.packet.msg_size: {}\n", p.header().msg_size);
+	println!(">> in.packet.msg_type: {:?}\n", p.header().msg_type);
+	
 	
 	match p.header().msg_type {
 		DvspMsgType::GsnResponse => {
